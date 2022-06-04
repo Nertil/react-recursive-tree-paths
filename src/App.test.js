a41b726afefb2504d6ad8c2835761b7d7c014cd2
Nertil Qatipi <qatipi.nertil@gmail.com>
2022-06-04 21:39:03 +0200

    - project cleanup   - added prettier config file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/recursive tree paths/i);
+  expect(titleElement).toBeInTheDocument();
 });
